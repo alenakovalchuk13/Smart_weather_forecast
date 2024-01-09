958e94205b6522e8aab226575b0f4ca128a7644a
Alena Kovalchuk <alenakovalchuk13@gmail.com>
2024-01-09 22:54:22 +0300

    A project has been created

---
+++ src/main/java/com/example/smart_weather_forecast/SmartWeatherForecastApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_weather_forecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWeatherForecastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWeatherForecastApplication.class, args);
+    }
+
+}
